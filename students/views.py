aa2566e4ad18a7204d43366fef51b8bc57890dcd
katlegoprince98 <CynicsTech1998@gmail.com>
2023-03-23 05:34:41 +0200

    modals

--- students/views.py
+++ students/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Student
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -7,3 +9,6 @@ def index(request):
         'students': Student.objects.all()
     })
 
+def view_student(request, id):
+    student = Student.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
